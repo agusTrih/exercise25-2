c40327c399221e4cb652d988f1c30b8ef0d8e58b
agusTrih <ant3dong@gmail.com>
2020-06-19 15:25:07 +0700

    no2

---
+++ script1.js
@@ -0,0 +1,6 @@
+import { addition, subtraction, divided, multiply } from "./script3.js";
+
+console.log(addition(2, 4));
+console.log(subtraction(2, 4));
+console.log(divided(2, 4));
+console.log(multiply(2, 4));
---
+++ script3.js
@@ -0,0 +1,13 @@
+function addition(a, b) {
+    return a + b;
+}
+function subtraction(a, b) {
+    return a - b;
+}
+function divided(a, b) {
+    return a / b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+export { addition, subtraction, divided, multiply };
